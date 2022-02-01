0e055136b59eb06bf7574d09dff05c3aed73d78a
nataliaradomska <nalivllk@gmail.com>
2022-02-01 01:03:24 +0100

    strona personelu

--- lib/screens/staff_home_screen.dart
+++ lib/screens/staff_home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:first_app/cloud_firestore/all_salon_ref.dart';
 import 'package:first_app/cloud_firestore/banner_ref.dart';
 import 'package:first_app/cloud_firestore/user_ref.dart';
+import 'package:first_app/model/city_model.dart';
 import 'package:first_app/model/image_model.dart';
+import 'package:first_app/model/salon_model.dart';
 import 'package:first_app/model/user_model.dart';
 import 'package:first_app/state/state_managment.dart';
 import 'package:flutter/material.dart';
@@ -13,12 +16,125 @@ import 'package:google_fonts/google_fonts.dart';
 class StaffHome extends ConsumerWidget{
   @override
   Widget build(BuildContext context, ref) {
+    var currentStaffStep = ref.watch(staffStep.state).state;
+    var cityWatch = ref.watch(selectedCity.state).state;
+    var salonWatch = ref.watch(selectedSalon.state).state;
+
     return SafeArea(child: Scaffold(
       resizeToAvoidBottomInset: true,
       backgroundColor: Color(0xFFDFDFDF),
-      appBar: AppBar(title: Text('Personel'),backgroundColor: Color(0xFF383838),),
-      body: Center(child: Text('Personel'),),
+      appBar: AppBar(title: Text(currentStaffStep == 1 ? 'Wybierz miasto' : currentStaffStep == 2
+          ? 'Wybierz salon' : currentStaffStep == 3 ? 'Twoj kalendarz' : 'Personel'),backgroundColor: Color(0xFF383838),),
+      body: Column(children: [
+
+        Expanded(child: currentStaffStep == 1 ? displayCity(ref) : currentStaffStep == 2 ? displaySalon(ref, cityWatch.name) : Container(), flex: 10,),
+        //button
+        Expanded(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                      child: ElevatedButton(
+                        onPressed: currentStaffStep == 1 ? null : ()=> ref.read(staffStep.state).state--,
+                        child: Text('Previous'),
+                      )),
+                  SizedBox(width: 30,),
+                  Expanded(
+                      child: ElevatedButton(
+                        onPressed: (currentStaffStep == 1
+                            && ref.read(selectedCity.state).state.name ==
+                                null) ||
+                            (currentStaffStep == 2 &&
+                                ref.read(selectedSalon.state).state.docId ==
+                                    null) ||
+                            currentStaffStep == 3
+                            ? null
+                            : ()=> ref.read(staffStep.state).state++,
+                        child: Text('Next'),
+                      )),
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+
+      ),
     ),
     );
+  }
+
+  displayCity(WidgetRef ref) {
+    return FutureBuilder(
+        future: getCities(),
+        builder: (context,snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting)
+            return Center(child: CircularProgressIndicator(),);
+          else{
+            var cities = snapshot.data as List<CityModel>;
+            if(cities == null || cities.length == 0)
+              return Center(child: Text('Cannot load city list'),);
+            else
+              return GridView.builder(
+                itemCount: cities.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+
+                  return GestureDetector(
+                    onTap: () =>  ref.read(selectedCity.state).state = cities[index],
+                    child: Padding(padding: const EdgeInsets.all(8),
+                    child: Card(
+                      shape: ref.read(selectedCity.state).state.name == cities[index].name ?
+                          RoundedRectangleBorder(side: BorderSide(color:Colors.green,width: 4),
+                          borderRadius: BorderRadius.circular(5)) : null,
+                      child: Center(child: Text('${cities[index].name}')),
+                    )),
+                  );
+
+                  });
+          }
+
+        });
+
+  }
+
+  displaySalon(WidgetRef ref, String name) {
+
+    return FutureBuilder(
+        future: getSalonByCity(name),
+        builder: (context,snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting)
+            return Center(child: CircularProgressIndicator(),);
+          else{
+            var salons = snapshot.data as List<SalonModel>;
+            if(salons == null || salons.length == 0)
+              return Center(child: Text('Cannot load Salon list'),);
+            else
+              return  ListView.builder(
+                  itemCount: salons.length,
+                  itemBuilder:(context,index){
+                    return GestureDetector(onTap: ()=> ref.read(selectedSalon.state).state = salons[index],
+                      child: Card(child: ListTile(
+                        shape: ref.read(selectedSalon.state).state.name == salons[index].name ?
+                        RoundedRectangleBorder(side: BorderSide(color:Colors.green,width: 4),
+                            borderRadius: BorderRadius.circular(5)) : null,
+                        leading: Icon(Icons.home_outlined, color: Colors.black,
+                        ),
+                        trailing: ref.read(selectedSalon.state).state.docId ==
+                            salons[index].docId
+                            ? Icon(Icons.check)
+                            : null,
+                        title: Text('${salons[index].name}', style: GoogleFonts.robotoMono(),),
+                        subtitle: Text('${salons[index].address}', style: GoogleFonts.robotoMono(fontStyle: FontStyle.italic),),
+                      ),),);
+                  });
+          }
+
+        });
+
   }
 }
--- lib/state/state_managment.dart
+++ lib/state/state_managment.dart
@@ -22,5 +22,8 @@ final selectedDate = StateProvider ((ref)=> DateTime.now());
 final selectedTimeSlot = StateProvider ((ref)=> -1);
 final selectedTime = StateProvider ((ref)=> '');
 
-//Deleting Books
+//Deleting Booking
 final deleteFlagRefresh = StateProvider((ref) => false);
+
+//Personel
+final staffStep = StateProvider((ref) => 1);
